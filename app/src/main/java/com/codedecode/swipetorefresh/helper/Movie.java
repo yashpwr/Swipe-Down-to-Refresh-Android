5f21ef5caf7909faaf3de936c6f73f6c98744173
yash <yashdvn@gmail.com>
2020-04-24 18:51:12 +0530

    initial commit

---
+++ app/src/main/java/com/codedecode/swipetorefresh/helper/Movie.java
@@ -0,0 +1,11 @@
+package com.codedecode.swipetorefresh.helper;
+
+public class Movie {
+    public int id;
+    public String title;
+
+    public Movie(int id, String title) {
+        this.title = title;
+        this.id = id;
+    }
+}
